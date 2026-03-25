7c4f8ac46cb12a7f0ec1af20003ca84eb82dcc52
dovid-moshe-crow <netanel.zevin@gmail.com>
2026-03-25 17:09:29 +0200

    Fix missing Link import in Neon page

--- app/neon/page.js
+++ app/neon/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ENUM_LABELS } from "../../lib/student-fields";
 import { getCurrentAppUser } from "../../lib/rbac";
